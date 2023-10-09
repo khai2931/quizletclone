ad24d454c4d141e2c2688000f4902a79312b764d
Khai-Huy Nguyen <khai2931@cs.washington.edu>
2023-10-09 15:32:30 -0700

    better comments

--- src/Main.java
+++ src/Main.java
@@ -54,25 +54,15 @@ class Main {
         System.out.println();
         input = new Scanner(System.in);
         rand = new Random();
-        System.out.println("Default settings:");
-        System.out.println("answer with definition, ignore casing, randomize questions, ignore parentheses.");
+        printSettings();
         System.out.print("Would you like to adjust settings? (y/n) ");
         String setSettings = input.nextLine();
         System.out.println();
         if (setSettings.equals("y")) {
-            System.out.print("Answer with definition or term? (d for definition, t for term) ");
-            ANSWER_WITH_DEFINITION = input.nextLine().equals("d");
-            System.out.println();
-            System.out.print("Require exact casing for correct answer? (y/n) ");
-            CASE_SENSITIVE = input.nextLine().equals("y");
-            System.out.println();
-            System.out.print("Randomize the order of questions? (y/n) ");
-            RANDOMIZE = input.nextLine().equals("y");
-            System.out.println();
-            System.out.print("Ignore parentheses and anything inside them when grading? (y/n) ");
-            IGNORE_PARENTHESES = input.nextLine().equals("y");
-            System.out.println();
+            adjustSettings();
         }
+        printSettings();
+        System.out.println();
         boolean isStudying = true;
         while (isStudying) {
             if (TERMS_FILE == null) {
@@ -103,7 +93,45 @@ class Main {
         }
     }
 
-    public static List<String> parseTerms(String filename) {
+    /**
+     * Prints the current settings.
+     */
+    private static void printSettings() {
+        System.out.println("Current settings:");
+        String ansWithDef = ANSWER_WITH_DEFINITION ? "answer with definition" : "answer with term";
+        String ignoreCase = CASE_SENSITIVE ?        "case sensitive" :          "ignore casing";
+        String randomQ    = RANDOMIZE ?             "randomize questions" :     "keep questions in file's order";
+        String ignorePar  = IGNORE_PARENTHESES ?    "ignore parentheses" :      "don't ignore parentheses";
+
+        System.out.println(ansWithDef + ", " + ignoreCase + ", " + randomQ + ", " + ignorePar + ".");
+    }
+
+    /**
+     * Adjusts the current settings.
+     */
+    private static void adjustSettings() {
+        System.out.print("Answer with definition or term? (d for definition, t for term) ");
+        ANSWER_WITH_DEFINITION = input.nextLine().equals("d");
+        System.out.println();
+        System.out.print("Require exact casing for correct answer? (y/n) ");
+        CASE_SENSITIVE = input.nextLine().equals("y");
+        System.out.println();
+        System.out.print("Randomize the order of questions? (y/n) ");
+        RANDOMIZE = input.nextLine().equals("y");
+        System.out.println();
+        System.out.print("Ignore parentheses and anything inside them when grading? (y/n) ");
+        IGNORE_PARENTHESES = input.nextLine().equals("y");
+        System.out.println();
+    }
+
+    /**
+     * Parses the file into term/definition pairs.
+     *
+     * @param filename The file to parse
+     * @return A list of "pairs" of terms/definitions as
+     * described in the class comment
+     */
+    private static List<String> parseTerms(String filename) {
         List<String> terms = null;
         try {
             File file = new File(filename);
@@ -124,7 +152,14 @@ class Main {
         }
     }
 
-    public static void testTerms(List<String> terms) {
+    /**
+     * (Possibly repeatedly) tests the user on the list of pairs.
+     * While the user answers any questions wrong, the user is given
+     * an option to test through all the questions they got wrong.
+     *
+     * @param terms list of term/definition pairs to use
+     */
+    private static void testTerms(List<String> terms) {
         String userAnswer = "";
         List<String> remainingTerms = new ArrayList<>(terms);
         List<String> incorrectTerms = new ArrayList<>();
@@ -136,7 +171,7 @@ class Main {
             userAnswer = input.nextLine();
             System.out.println();
             if (userAnswer.equals("y")) {
-                pageBreak();
+                pageBreak(); // to prevent user from copying correct answers already seen
                 remainingTerms = incorrectTerms;
                 incorrectTerms = new ArrayList<>();
                 numCorrect = goThroughTerms(remainingTerms, incorrectTerms);
@@ -148,6 +183,15 @@ class Main {
         }
     }
 
+    /**
+     * Tests the user on the list of pairs.
+     * Note that the behavior of order of terms and what is considered "correct" is modified
+     * by the above settings/public constants. See the top of this class for more detail.
+     *
+     * @param remainingTerms list of term/definition pairs to use
+     * @param incorrectTerms list of pairs that the user got wrong
+     * @return the number of questions the user answered correctly
+     */
     private static int goThroughTerms(List<String> remainingTerms, List<String> incorrectTerms) {
         int numCorrect = 0;
         int numComplete = 0;
@@ -208,6 +252,13 @@ class Main {
         return numCorrect;
     }
 
+    /**
+     * Returns whether the answer is correct.
+     *
+     * @param userAnswer what the user answered
+     * @param answer what the actual answer is
+     * @return true if the answer is correct, false otherwise
+     */
     private static boolean answerIsCorrect(String userAnswer, String answer) {
         if (!CASE_SENSITIVE) {
             userAnswer = userAnswer.toLowerCase();
@@ -223,6 +274,16 @@ class Main {
         return userAnswer.equals(answer);
     }
 
+    /**
+     * Given a comma-separated list of items,
+     * returns an alphabetically ordered comma-separated list of those items
+     * with each item trimmed of any leading/trailing whitespace.
+     *
+     * @param str comma-separated list of items (or no comma for one item)
+     * @return alphabetically ordered comma-separated list of those items
+     * with each item trimmed of any leading/trailing whitespace.
+     * (as a string)
+     */
     private static String alphabeticalNoComma(String str) {
         String[] items = str.split(",");
         for (int i = 0; i < items.length; i++) {
@@ -232,6 +293,15 @@ class Main {
         return String.join(",", items);
     }
 
+    /**
+     * Given a string,
+     * returns the same string with all text inside parentheses removed,
+     * all parentheses removed, and finally trimmed.
+     *
+     * @param str any string
+     * @return the same string with all text inside parentheses removed,
+     * all parentheses removed, and finally trimmed.
+     */
     private static String deleteParens(String str) {
         int[] pair = getFirstParenPair(str);
         while (pair != null) {
@@ -244,6 +314,20 @@ class Main {
         return str.trim();
     }
 
+    /**
+     * Given a string, finds the first pair of parentheses in the string
+     * that has NO parentheses between them.
+     * Returns null if no such pair is found.
+     * Returns an int[] such that int[0] is the index of the left parentheses in that pair,
+     * and int[1] is the index of the right parentheses in that pair,
+     * only if that pair is found.
+     *
+     * @param str any string
+     * @return null if no such pair (above) is found. Otherwise, returns an
+     * int[] such that int[0] is the index of the left parentheses in that pair,
+     * and int[1] is the index of the right parentheses in that pair,
+     * only if that pair is found.
+     */
     private static int[] getFirstParenPair(String str) {
         if (str.contains("(")) {
             int left = str.indexOf("(");
@@ -259,16 +343,31 @@ class Main {
         return null;
     }
 
+    /**
+     * Given a string, returns the "leftmost" token after splitting by tab characters.
+     *
+     * @param str any string
+     * @return the "leftmost" token after splitting by tab characters.
+     */
     private static String left(String str) {
         String[] tokens = str.split("\t");
         return tokens[0].trim();
     }
 
+    /**
+     * Given a string, returns the "rightmost" token after splitting by tab characters.
+     *
+     * @param str any string
+     * @return the "rightmost" token after splitting by tab characters.
+     */
     private static String right(String str) {
         String[] tokens = str.split("\t");
         return tokens[tokens.length - 1].trim();
     }
 
+    /**
+     * Prints 100 newlines.
+     */
     private static void pageBreak() {
         for (int i = 0; i < 100; i++) {
             System.out.println();
